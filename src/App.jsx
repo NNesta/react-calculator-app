19f05a7ea957ff6dffbd3ad942516eda3b7e0cb4
Ngabonziza Nestor <ngabonest@gmail.com>
2023-02-15 15:35:51 +0200

    feat: adding ui of alculator

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+function App() {
+  const [value, setValue] = useState('')
+  const [variables, setVariables] = useState([])
+  useEffect(()=>{
+   setVariables([...variables, value])
+  },[value])
+  return (
+    
+  <div className='max-w-md mx-auto text-4xl font-bold h-[80vh] mt-8 '>
+   <div className='bg-[#7B7A89] text-white text-left h-24 py-6'>
+    <p className='text-right'>{variables}</p>
+   </div>
+   <div className='grid grid-cols-4 h-full'>
+   <button value='AC' onClick={event=>setVariables('')} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>AC</button>
+   <button value='+/-' onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>+/-</button>
+   <button value='%' onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>%</button>
+   <button value='/' onClick={event=>setValue(event.target.value)} className='text-black inline-flex justify-center items-center bg-[#F48637] border border-black'>/</button>
+   <button value={7} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>7</button>
+   <button value={8} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>8</button>
+   <button value={9} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>9</button>
+   <button value='*' onClick={event=>setValue(event.target.value)} className='text-black  inline-flex justify-center items-center bg-[#F48637] border border-black'>X</button>
+   <button value={4} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>4</button>
+   <button value={5} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>5</button>
+   <button value={6} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>6</button>
+   <button value='-' onClick={event=>setValue(event.target.value)} className='text-black inline-flex justify-center items-center bg-[#F48637] border border-black'>-</button>
+   <button value={1} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>1</button>
+   <button value={2} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>2</button>
+   <button value={3} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>3</button>
+   <button value='+' onClick={event=>setValue(event.target.value)} className='text-black  inline-flex justify-center items-center bg-[#F48637] border border-black'>+</button>
+   <button value={0} onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center col-span-2 border border-black'>0</button>
+   <button value='.' onClick={event=>setValue(event.target.value)} className='text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black'>.</button>
+   <button value='=' onClick={()=>setVariables(eval(variables.join('')))} className='text-black  inline-flex justify-center items-center bg-[#F48637] border border-black'>=</button>
+   </div>
+  </div>
+  );
+}
+
+export default App;
