2f62afa2892653e099331af8003914d04a0aa3bb
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-04 17:28:03 +0200

    feat:fixing issue on equal sign

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,79 @@
 import React, { useState } from "react";
+import data from "./data";
 
 function App() {
   const [first, setFirst] = useState(null);
   const [second, setSecond] = useState("");
   const [prev, setPrev] = useState(null);
+  const [sign, setSign] = useState("+");
+
   const calculate = (op) => {
     if (prev === null || first === null) {
-      console.log(prev, first);
       setFirst(+second);
       setSecond("");
       setPrev(op);
       return;
     }
-    console.log(prev);
     switch (prev) {
       case "+":
-        setFirst(first + +second);
+        setFirst(first + Number(second));
         break;
       case "-":
-        setFirst(first - +second);
+        setFirst(first - Number(second));
         break;
       case "*":
-        setFirst(first * +second);
+        setFirst(first * Number(second));
         break;
       case "/":
-        setFirst(first / +second);
+        setFirst(first / Number(second));
+        break;
+      case "=":
         break;
     }
     setPrev(op);
     setSecond("");
   };
+  const handleClick = (event) => {
+    const input = event.target.value;
+    switch (input) {
+      case "+":
+        calculate("+");
+        break;
+      case "/":
+        calculate("/");
+        break;
+      case "*":
+        calculate("*");
+        break;
+      case "-":
+        calculate("-");
+        break;
+      case "AC":
+        setSecond("");
+        setFirst(0);
+        setPrev(null);
+        break;
+      case "+/-":
+        setSecond((prev) => -prev);
+        break;
+      case "=":
+        calculate("=");
+        break;
+      default:
+        setSecond((prev) => prev + input);
+    }
+  };
   return (
     <div className="max-w-md mx-auto text-5xl font-semibold h-[80vh] mt-8 ">
       <div className="bg-[#7B7A89] text-white text-left h-24 py-6 border border-black">
-        <p className="text-right">{second || first}</p>
+        <p className="text-right">{+second || +first}</p>
       </div>
       <div className="grid grid-cols-4 h-full">
-        {[
-          { id: 1, value: "AC" },
-          { id: 2, value: "+/-" },
-          { id: 3, value: "%" },
-          { id: 4, value: "/" },
-          { id: 5, value: 7 },
-          { id: 6, value: 8 },
-          { id: 7, value: 9 },
-          { id: 8, value: "*" },
-          { id: 9, value: 4 },
-          { id: 10, value: 5 },
-          { id: 11, value: 6 },
-          { id: 12, value: "-" },
-          { id: 13, value: 1 },
-          { id: 14, value: 2 },
-          { id: 15, value: 3 },
-          { id: 16, value: "+" },
-          { id: 17, value: 0 },
-          { id: 18, value: "." },
-          { id: 19, value: "=" },
-        ].map((item, index) => (
+        {data.map((item, index) => (
           <button
             key={index}
             value={item.value}
-            onClick={(event) => {
-              const input = event.target.value;
-              switch (input) {
-                case "=":
-                  calculate("+");
-                  break;
-                case "+":
-                  calculate("+");
-                  break;
-                case "/":
-                  calculate("/");
-                  break;
-                case "*":
-                  calculate("*");
-                  break;
-                case "-":
-                  calculate("-");
-                  break;
-                case "AC":
-                  setSecond("");
-                  setFirst(0);
-                  break;
-                default:
-                  setSecond((prev) => prev + input);
-              }
-            }}
+            onClick={handleClick}
             className={`text-black bg-[#DCDBDC] inline-flex justify-center items-center border border-black ${
               item.id == 17 && "col-span-2"
             } ${[4, 8, 12, 16, 19].includes(item.id) && "bg-[#F48637]"}`}
